08c461031ba03cc50a84f2b14de080bc614158ea
stexus <massimo.tseng@gmail.com>
2020-09-13 13:30:27 -0700

    first commit

---
+++ betteranimecards.js
@@ -0,0 +1,11 @@
+const AUDIO_THRESHOLD = 0.25; // 250ms
+const IMAGE_WIDTH = 520;
+const IMAGE_HEIGHT = 520;
+const IMAGE_DELAY_PERCENT = 0.08;
+const ANKI_DECK_NAME = 'Manual Mine';
+const ANKI_NOTE_TYPE = 'Audio Cards';
+const ANKI_TAG_NAME = 'animecards';
+const ANKI_MEDIA_COLLECTION =
+  process.env.HOME + '/.local/share/Anki2/User 1/collection.media';
+const CURL = 'curl';
+
