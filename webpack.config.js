e414f2285eea0fe07eaccd6bdf755c44b8f5bfea
stexus <massimo.tseng@gmail.com>
2020-10-17 19:10:53 -0700

    fix webpack environment variable fetching

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   // Change to your "entry-point".
@@ -35,4 +36,5 @@ module.exports = {
       },
     ],
   },
+  plugins: [new webpack.EnvironmentPlugin(["HOME"])],
 };
