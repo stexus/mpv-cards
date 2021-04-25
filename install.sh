aaede690dbcb05b3aa2dcbf1d0a21c599efa3ea8
stexus <massimo.tseng@gmail.com>
2021-04-24 18:21:29 -0700

    create mpv scripts directory if missing in installation script

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 MPV_SCRIPTS_DIR="$HOME/.config/mpv/scripts"
 
- cp dist/main.bundle.js "$MPV_SCRIPTS_DIR"/main.bundle.js  
+mkdir -p "$MPV_SCRIPTS_DIR"
+cp dist/main.bundle.js "$MPV_SCRIPTS_DIR"/main.bundle.js  
